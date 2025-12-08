f6e9167fcf641f987e7e49ebfef4c380d7188d62
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-12-08 19:40:30 +0300

    Добавлен сервис работы с переменными окружения

---
+++ backend/config/env.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+// Инициализация загрузки файла окружения
+func Init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("WARNING: No .env file")
+	}
+	log.Println(".env file loaded")
+}
+
+// Работа со строковыми значениями
+func getString(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+// Работа с числовыми значениями
+func getInt(key string, defaultValue int) int {
+	value := os.Getenv(key)
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+	return i
+}
+
+// Работа с булево значениями
+func getBool(key string, defaultValue bool) bool {
+	value := os.Getenv(key)
+	b, err := strconv.ParseBool(value)
+	if err != nil {
+		return defaultValue
+	}
+	return b
+}
+
+// Структура конфигурации БД
+type DatabaseConfig struct {
+	Url string
+}
+
+// Извление конфига БД из переменной окружения
+func NewDatabaseConfig() *DatabaseConfig {
+	return &DatabaseConfig{
+		Url: getString("POSTGRES_DSN", ""),
+	}
+}
