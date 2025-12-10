baf0b686b5e6a0fb28c4f68aee4deb5a3d46b553
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-12-10 16:02:34 +0300

    Добавлено разделение авторизации для mailhog и prod

--- backend/pkg/mail/service.go
+++ backend/pkg/mail/service.go
@@ -2,6 +2,7 @@ package mail
 
 import (
 	"fmt"
+	"net"
 	"net/smtp"
 	"strings"
 
@@ -15,18 +16,37 @@ type SMTPClient struct {
 	Auth smtp.Auth
 }
 
-func NewSMTPClient(auth smtp.Auth) *SMTPClient {
-	SMTPConfig := config.NewSMTPConfig()
-	return &SMTPClient{
-		Host: SMTPConfig.Host,
-		Port: SMTPConfig.Port,
-		From: SMTPConfig.User,
-		Auth: auth,
+func NewSMTPClient() (*SMTPClient, error) {
+	// Получаем конфиг SMTP
+	cfg := config.NewSMTPConfig()
+	// Проверяем, что хост и порт не пустые
+	if cfg.Host == "" || cfg.Port == "" {
+		return nil, fmt.Errorf("SMTP_HOST and SMTP_PORT are required")
 	}
+	// Если пользователь и пароль не пустые, то создаем auth
+	var auth smtp.Auth
+	if cfg.User != "" || cfg.Password != "" {
+		auth = smtp.PlainAuth("", cfg.User, cfg.Password, cfg.Host)
+	}
+
+	// Создаем SMTPClient
+	return &SMTPClient{
+		Host: cfg.Host,
+		Port: cfg.Port,
+		From: cfg.User,
+		Auth: auth,
+	}, nil
 }
 
 func (c *SMTPClient) SendMail(to []string, subject, body string) error {
-	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", strings.Join(to, ", "), subject, body))
-	addr := c.Host + ":" + c.Port
+	headers := []string{
+		"From: " + c.From,
+		"To: " + strings.Join(to, ", "),
+		"Subject: " + subject,
+		"MIME-Version: 1.0",
+		"Content-Type: text/html; charset=\"UTF-8\"",
+	}
+	msg := []byte(strings.Join(headers, "\r\n") + "\r\n\r\n" + body)
+	addr := net.JoinHostPort(c.Host, c.Port)
 	return smtp.SendMail(addr, c.Auth, c.From, to, msg)
 }
